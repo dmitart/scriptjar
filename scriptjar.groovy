eea61e4892477e0598b55d25445b8498d3cfcc88
Dmitry Artjomenko <dmitry.artjomenko@ask.fm>
2015-11-14 17:12:43 +0200

    multiple classes support

--- scriptjar.groovy
+++ scriptjar.groovy
@@ -9,10 +9,13 @@ import org.codehaus.groovy.control.CompilationUnit
 import org.codehaus.groovy.control.CompilationUnit.PrimaryClassNodeOperation
 import org.codehaus.groovy.control.SourceUnit
 import org.codehaus.groovy.tools.GroovyClass
+
 import java.util.jar.JarEntry
 import java.util.jar.JarFile
 import java.util.jar.JarOutputStream
-import static org.codehaus.groovy.control.Phases.*
+
+import static org.codehaus.groovy.control.Phases.CLASS_GENERATION
+import static org.codehaus.groovy.control.Phases.SEMANTIC_ANALYSIS
 
 class RemoveStaticPrimaryClassNodeOperation extends PrimaryClassNodeOperation {
 
@@ -45,21 +48,22 @@ class RemoveStaticPrimaryClassNodeOperation extends PrimaryClassNodeOperation {
     }
 }
 
-def compile(String prefix, File file) {
+List<GroovyClass> compile(String prefix, File file) {
     CompilationUnit unit = new CompilationUnit()
     unit.addPhaseOperation(new RemoveStaticPrimaryClassNodeOperation(), SEMANTIC_ANALYSIS)
     unit.addSource(SourceUnit.create(prefix, file.text))
     unit.compile(CLASS_GENERATION)
-    GroovyClass gc = unit.getClasses()[0]
-    return gc.getBytes()
+    return unit.getClasses()
 }
 
-File getGroovyLib() {
+List<File> getGroovyLibs(List neededJars) {
     if (System.getenv('GROOVY_HOME')) {
         def libs = new File(System.getenv('GROOVY_HOME'), 'lib')
-        def groovylib = libs.listFiles().find{it.name =~ /groovy-\d+.\d+.\d+.jar/}
-        if (groovylib) {
-            return groovylib
+        def groovylibs = libs.listFiles().findAll{jar ->
+            neededJars.any{needed -> jar.name =~ needed  }
+        }
+        if (groovylibs) {
+            return groovylibs
         } else {
             println "Cann't find Groovy lib in ${libs.absolutePath}, specify it manually as Grab dependency"
             System.exit(1)
@@ -70,29 +74,27 @@ File getGroovyLib() {
     }
 }
 
-List dependencies(String name) {
+List dependencies(File source) {
     final GroovyClassLoader classLoader = new GroovyClassLoader()
-    classLoader.parseClass(new File(name).text)
+    classLoader.parseClass(source.text)
     def files = Grape.resolve([:], Grape.listDependencies(classLoader)).collect{ new JarFile(it.path) }
-    files << new JarFile(getGroovyLib())
+    files.addAll(getGroovyLibs([/groovy-\d+.\d+.\d+.jar/]).collect{ new JarFile(it) })
+    return files
 }
 
-void createJar(String prefix, String name, List jars, byte[] compiled) {
-    File output = new File(name)
-    JarOutputStream jos = new JarOutputStream(new FileOutputStream(output))
+void writeJarEntry(JarOutputStream jos, JarEntry entry, byte[] data) {
+    entry.setSize(data.length)
+    jos.putNextEntry(entry)
+    jos.write(data)
+}
+
+byte[] createJar(String prefix, List jars, List<GroovyClass> compiled) {
+    ByteArrayOutputStream output = new ByteArrayOutputStream()
+    JarOutputStream jos = new JarOutputStream(output)
 
     jos.putNextEntry(new JarEntry('META-INF/'))
-
-    def manifestEntry = new JarEntry('META-INF/MANIFEST.MF')
-    byte[] manifest = "Manifest-Version: 1.0\nMain-Class: ${prefix}\n".getBytes()
-    manifestEntry.setSize(manifest.length)
-    jos.putNextEntry(manifestEntry)
-    jos.write(manifest)
-
-    JarEntry main = new JarEntry("${prefix}.class")
-    main.setSize(compiled.length)
-    jos.putNextEntry(main)
-    jos.write(compiled)
+    writeJarEntry(jos, new JarEntry('META-INF/MANIFEST.MF'), "Manifest-Version: 1.0\nMain-Class: ${prefix}\n".getBytes())
+    compiled.each { writeJarEntry(jos, new JarEntry("${it.name}.class"), it.bytes) }
 
     def directories = ['META-INF/', 'META-INF/MANIFEST.MF']
 
@@ -100,19 +102,31 @@ void createJar(String prefix, String name, List jars, byte[] compiled) {
         println "Merging ${file.name}"
         file.entries().each { JarEntry entry ->
             if (!directories.contains(entry.name)) {
-                byte[] arr = file.getInputStream(entry).getBytes()
-                jos.putNextEntry(entry)
-                jos.write(arr)
+                writeJarEntry(jos, entry, file.getInputStream(entry).getBytes())
                 directories << entry.name
             }
         }
     }
 
     jos.close()
+    return output.toByteArray()
+}
+
+byte[] createUberjar(File file, String prefix) {
+    List<GroovyClass> compiled = compile(prefix, file)
+    def jars = dependencies(file)
+    return createJar(prefix, jars, compiled)
+}
+
+
+if (args.size() < 2) {
+    println "Usage: ./scriptjar.groovy input.groovy output.jar"
+    System.exit(1)
 }
 
 File file = new File(args[0])
 String prefix = file.name.substring(0, file.name.indexOf('.'))
-byte[] compiled = compile(prefix, file)
-def jars = dependencies(args[0])
-createJar(prefix, args[1], jars, compiled)
+
+new File(args[1]).withOutputStream {
+    it << createUberjar(file, prefix)
+}
